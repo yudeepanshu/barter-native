22d5b5aba5c79e8a023a051b4234ac421cad1062
yudeepanshu <waiker.deepanshu@gmail.com>
2026-06-03 03:53:46 +0530

    feat: app version bump

--- app.config.ts
+++ app.config.ts
@@ -5,7 +5,7 @@ const IS_DEV = process.env.EXPO_PUBLIC_APP_ENV === "dev";
 export default (): ExpoConfig => ({
   name: "Flippe",
   slug: "flippe",
-  version: "1.0.0",
+  version: "1.0.1",
   scheme: "flippe",
   orientation: "portrait",
   icon: "./assets/icon.png",
@@ -25,7 +25,7 @@ export default (): ExpoConfig => ({
   },
   android: {
     package: "com.shinichi.flippeapp",
-    versionCode: 62,
+    versionCode: 63,
     googleServicesFile: "./google-services.json",
     adaptiveIcon: {
       foregroundImage: "./assets/adaptive-icon.png",
